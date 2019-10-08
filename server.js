da2e66c0ba1168b64c9680bfeb23aa8dd359b593
Ravi Mantur <RaviMantur@RavisMac.local>
2019-10-08 17:24:53 -0400

    added express server and separate package.json... moved client into client folder

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("client/public"));
+
+app.listen(PORT, function() {
+  console.log("App listening on port" + PORT + "\nhttps://localhost:" + PORT);
+});
