a8ca8d442692ffe4ac72298cd39a0fc768ccfc6d
Ravi Mantur <RaviMantur@RavisMac.local>
2019-10-08 18:17:12 -0400

    added html-routes

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("client/public"));
 
+require("./routes/html-routes")(app);
+
 app.listen(PORT, function() {
   console.log("App listening on port" + PORT + "\nhttps://localhost:" + PORT);
 });
