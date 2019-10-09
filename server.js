d794ca1a112ef506e92207a250be0b145c803f07
Ravi Mantur <RaviMantur@RavisMac.local>
2019-10-09 09:03:59 -0400

    changed the path

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(express.static("client/public"));
-app.use("/static", express.static(path.join(__dirname, "client/public")));
+// app.use("/static", express.static(path.join(__dirname, "client/public")));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
 
 require("./routes/html-routes")(app);
 
