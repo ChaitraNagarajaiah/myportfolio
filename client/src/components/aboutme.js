69344ccc253c9dbb48fe306aba826f5a8c35111c
Ravi Mantur <RaviMantur@RavisMac.local>
2019-10-09 19:29:48 -0400

    changed about me content

--- client/src/components/aboutme.js
+++ client/src/components/aboutme.js
@@ -35,18 +35,19 @@ class AboutMe extends Component {
             >
               Chaitra Nagarajaiah
             </h4>
-            Hello, I'am Chaitra from Atlanta. I persuded my degree in
-            Engineering in Visveswaraya Technological University in India.
-            Currently I have completed Full Stack development course from
-            Georgia Tech, Atlanta. My hobbies are painting, drawing, cooking and
-            gardening. Full Stack Web Developer with a multidisciplinary
-            background in Engineering providing strong problem-solving skills
-            with a keen eye for aesthetics and presentation. An excellent
-            communicator and collaborator who works well in team-based projects
-            and independently with very strong interpersonal skills. With each
+            Hello, I'am Chaitra from Atlanta. I have attained Bachelors degree
+            in Engineering at Visveswaraya Technological University from India.
+            I like to learn new things and keen on keeping myself updated with
+            latest technology and trend which motivated me to gain Full Stack
+            development certificate from Georgia Tech University, Atlanta. My
+            hobbies are painting, drawing, cooking and gardening. As a Full
+            Stack Web Developer I have a strong problem-solving skills with a
+            keen eye for aesthetics and presentation. An excellent communicator
+            and collaborator who works well in team-based projects and
+            independently with very strong interpersonal skills. With each
             project, the aim is to provide simple robust solutions to problems
             specified by the client. Effectively leverage interest in solving
-            complex problems by contributing as a part of a fast-paced,
+            complex problems by contributing as part of a fast-paced,
             quality-driven team.
           </p>
         </Grid>
